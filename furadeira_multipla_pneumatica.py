b04d8b5f46a1caa519a4729ebd70667f63cc5d01
WellTrind <166227719+WellTrind@users.noreply.github.com>
2024-04-07 16:14:55 -0300

    Update furadeira_multipla_pneumatica.py

--- furadeira_multipla_pneumatica.py
+++ furadeira_multipla_pneumatica.py
@@ -12,3 +12,21 @@ estado_inicial = "q0" # definição do estado inicial.
 estado_final = {"q0"} # definição do estado final.
 
 automato = DFA(Q, sigma, delta, estado_inicial, estado_final)
+
+automato.is_valid()
+
+print ("Automato aceita alfabeto 1110: ", automato.accept("1110")) # funcionamento completo da máquina.
+
+print("Automato aceita alfabeto 110: ", automato.accept("110")) # funcionamento parcial da máquina.
+
+print("Automato aceita alfabeto 0000: ", automato.accept("0000")) #Máquina em estado de repouso
+
+print("Automato aceita alfabeto 1111: ", automato.accept("1111")) #Máquina em loop de perfuração
+
+automato.view("afd01")
+
+automato.view(
+    file_name="afd01_personalizado",
+    node_attr={'fontsize': '20'},
+    edge_attr={'fontsize': '20pt'}
+) # geração de um PNG do AFD.
